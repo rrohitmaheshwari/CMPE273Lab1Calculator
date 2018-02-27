ae3c7ef46a0065bb44b3135161315cd731ca0622
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-02-26 18:39:16 -0800

    Fist Commit for React App
    Creating general structure to begin with.

--- CalculatorNodeServer/routes/index.js
+++ CalculatorNodeServer/routes/index.js
@@ -2,31 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Calulator App'});
 });
 
-
-
 /* POST Calculator query. */
-router.post('/evaluate', function(req, res, next) {
-
-    console.log("*******");
-    console.log(req.body);
-    console.log(req.body.data);
-
+router.post('/evaluate', function (req, res, next) {
     try {
-        var result=eval(req.body.data);
+        var result = eval(req.body.data);
         console.log(result);
         res.status(201).send({message: result})
 
     }
-    catch(e)
-    {
+    catch (e) {
         console.log(e);
         res.status(401).send({message: 'Illegal expression'});
     }
-
-
 });
 module.exports = router;
---
+++ reactapp/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1 >Welcome to Calculator App</h1>
+        </header>
+        <p>
+          Lets get started
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
