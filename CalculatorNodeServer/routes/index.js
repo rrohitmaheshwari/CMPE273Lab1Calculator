80803476b8f480bb665fb14ea898dece4991035a
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-02-26 17:47:40 -0800

    Created POST request
    Created Post request which take expressions as input and return evaluted result.
    Also check basic expression validation.
    Enabled Cross-Origin Resource Sharing (CORS).

--- CalculatorNodeServer/routes/index.js
+++ CalculatorNodeServer/routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+/* POST Calculator query. */
+router.post('/evaluate', function(req, res, next) {
+
+    console.log("*******");
+    console.log(req.body);
+    console.log(req.body.data);
+
+    try {
+        var result=eval(req.body.data);
+        console.log(result);
+        res.status(201).send({message: result})
+
+    }
+    catch(e)
+    {
+        console.log(e);
+        res.status(401).send({message: 'Illegal expression'});
+    }
+
+
+});
 module.exports = router;
