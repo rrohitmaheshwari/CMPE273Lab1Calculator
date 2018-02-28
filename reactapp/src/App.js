299ab7e456b2505d2572fc8b3c00c3b0b1143036
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2018-02-28 13:23:08 -0800

    Creating React Basic Display div
    Basic UI done

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,19 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import * as API from './api/API.js';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h1 >Welcome to Calculator App</h1>
-        </header>
-        <p>
-          Lets get started
-        </p>
-      </div>
-    );
-  }
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: ' '
+        };
+
+
+    }
+
+    result() {
+
+        API.doPost(this.state)
+            .then((message) => {
+                console.log(message.message);
+                if (message.message == null)
+                    this.setState({data: ' Infinity'})
+                else
+                    this.setState({data: ' ' + message.message})
+            });
+
+
+    }
+
+    resetcal() {
+        this.setState({data: ' '})
+    }
+
+    updateState(val) {
+        if (this.state.data === ' Illegal expression' || this.state.data === ' Infinity')
+            this.setState({data: ' ' + val});
+        else
+            this.setState({data: this.state.data + val});
+        console.log(this.state.data);
+
+    }
+
+
+    render() {
+        return (
+            <div className="mx-auto">
+                <div className="calc">
+                    <div className="resulttext">
+                        <pre>{this.state.data}</pre>
+                    </div>
+
+                    <div className="row">
+                        <button className="calBut" onClick={this.updateState.bind(this, '7')}>7</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '8')}>8</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '9')}>9</button>
+                        <button className="calButControl" onClick={this.updateState.bind(this, '+')}>+</button>
+                    </div>
+
+                    <div className="row">
+                        <button className="calBut" onClick={this.updateState.bind(this, '4')}>4</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '5')}>5</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '6')}>6</button>
+                        <button className="calButControl" onClick={this.updateState.bind(this, '-')}>-</button>
+                    </div>
+
+                    <div className="row">
+                        <button className="calBut" onClick={this.updateState.bind(this, '1')}>1</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '2')}>2</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '3')}>3</button>
+                        <button className="calButControl" onClick={this.updateState.bind(this, '/')}>/</button>
+                    </div>
+
+                    <div className="row">
+                        <button className="calButControl" onClick={this.resetcal.bind(this)}>C</button>
+                        <button className="calBut" onClick={this.updateState.bind(this, '0')}>0</button>
+                        <button className="calButControl" onClick={this.result.bind(this)}>=</button>
+                        <button className="calButControl" onClick={this.updateState.bind(this, '*')}>*</button>
+                    </div>
+
+
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default App;
---
+++ reactapp/src/api/API.js
@@ -0,0 +1,27 @@
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:3001'
+
+const headers = {
+    'Accept': 'application/json'
+};
+
+export const doPost = (payload) =>
+    fetch(`${api}/index`, {
+        "crossDomain": true,
+        method: 'POST',
+        headers: {
+            ...headers,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload)
+    }).then(res => {
+        console.log(res);
+        return res.json();
+    })
+        .catch(error => {
+            console.log("This is error");
+            return error;
+        });
+
+
+
+
